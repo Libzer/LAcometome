7c36c1973ab66cc3c026065d946069785c39b3b8
Libzer <2fotopro@gmail.com>
2020-07-15 21:10:06 +0400

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Minecraft</title>
+    </head>
+    <body>
+        <h1>This site is for beginners in Minecraft </h1>
+    </body>
+</html>
